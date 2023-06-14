150fe3250e79ca63d53f69159413c5f90036fd37
Jonathan Wilde <jonathan@realworldprivacy.com>
2023-06-14 17:51:14 -0600

    [oss sprint|part 2] Bubble tool configuration up to the local module

--- packages/local/lib/server.ts
+++ packages/local/lib/server.ts
@@ -1,4 +1,27 @@
-import { server } from '@magnet-agent/core';
+import {
+  createServer,
+  FileReadTool,
+  DirectoryReadTool,
+  SearchTool,
+  FileCreationTool,
+  FileDeleteLinesTool,
+  FileInsertTextTool,
+  FileReplaceLinesTool,
+  FileDeletionTool,
+} from '@magnet-agent/core';
+
+const server = createServer({
+  tools: [
+    new FileReadTool(),
+    new DirectoryReadTool(),
+    new SearchTool(),
+    new FileCreationTool(),
+    new FileDeleteLinesTool(),
+    new FileInsertTextTool(),
+    new FileReplaceLinesTool(),
+    new FileDeletionTool(),
+  ],
+});
 
 server.listen({ port: 8080, host: '0.0.0.0' }, (err) => {
   console.log('Server running on port 8080');
