c7b0a194d9a26e9715f72d090800dd7b38a2084a
Jonathan Wilde <jonathan@realworldprivacy.com>
2023-06-28 18:34:33 -0600

    Export formatAgentResultOutput

--- magnet-agent/lib/host/index.ts
+++ magnet-agent/lib/host/index.ts
@@ -3,6 +3,7 @@ export { Host } from './Host';
 export { createDirectorySource } from './createDirectorySource';
 
 export { formatAgentResult } from './result/formatAgentResult';
+export { formatAgentResultOutput } from './result/formatAgentResultOutput';
 export { applyAgentResult } from './result/applyAgentResult';
 export { sendAgentResultFeedback } from './result/sendAgentResultFeedback';
 
